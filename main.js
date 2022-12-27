fbb1bba7704c02fa2745fcb54198c2c67df8cdac
Ignacio Farinella Queipo <nachofarinellaqueipo@gmail.com>
2022-12-26 22:24:07 -0300

    Preentrega3

--- main.js
+++ main.js
@@ -1,137 +1,75 @@
-//variables
+//Preentrega3
 
+//Quiero listar los clientes a medida que se vayan creando con los pedidos
 
-function validacionDatos (variable) {
-    while ( variable === null || variable === `` ) {
-        variable = prompt ("No se completó el campo. Ingrese el dato solicitado");
-        }
-}
+const formulario = document.querySelector("#altaCliente");
 
-let nombre = prompt("Por favor indique su nombre");
-
-validacionDatos (nombre);
-
-let apellido = prompt("Por favor indique su apellido");
-
-validacionDatos (apellido);
-
-let añoNacimiento = parseInt(prompt("Por favor indique su año de nacimiento"));
-
-validacionDatos (añoNacimiento);
-
-let producto = prompt("¿Qué producto desea ordenar?");
-
-validacionDatos (producto);
-
-let cantidadProducto = parseInt(prompt("¿Cuántas unidades desea?"));
-
-validacionDatos (cantidadProducto);
-
-let precioProducto = parseFloat(prompt("¿Cuál es el precio del producto?"));
-
-validacionDatos (precioProducto);
-
-let contraseña = parseInt(prompt("Ingrese una contraseña para su pedido, deben ser caracteres númericos"));
-
-validacionDatos (contraseña);
-
-let edad = parseInt(2022 - añoNacimiento);
-let nombreCompleto;
-let descuento;
-let pedidoCompleto;
-let precioPedido;
-let productosVendidos;
-
-
-//Ciclo-quiero solicitarle al cliente su contraseña para confirmarle el pedido
-
-let confirmacionPedido = parseInt(prompt("Por favor confirme su contraseña"));
-let intentos = 3;
-
-while ( confirmacionPedido !== contraseña && intentos > 0) {
-    intentos = intentos - 1;
-    alert("Su contraseña es incorrecta. Por favor intente de nuevo.");
-    if (intentos != 0){
-        confirmacionPedido = parseInt(prompt("Por favor confirme su contraseña"));
-        } else {
-        alert("No hemos podido confirmar su pedido. Contáctenos por teléfono al 4555-5555");
-        console.log("La contraseña ingresada fue incorrecta. Este pedido no está confirmado");
+class Cliente {
+    constructor(nombre, apellido, añoNacimiento, telefono){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.añoNacimiento = añoNacimiento;
+        this.telefono = telefono;
     }
 }
 
+let parrafoCliente = document.querySelector("#parrafoCliente");
+const clientesAgregados = JSON.parse(localStorage.getItem("clientes")) || [];
 
-//Condicional para determinar el descuento a aplicar
 
-if (cantidadProducto >= 3, cantidadProducto < 6) {
-    descuento = 0.9;
-} else if (cantidadProducto >= 6) {
-    descuento = 0.8;
-} else {
-    descuento = 1;
+const crearCliente = () => {
+    let nombre = document.getElementById("nombre").value;
+    let apellido = document.getElementById("apellido").value;
+    let añoNacimiento = parseInt(document.getElementById("añoNacimiento").value);
+    let telefono = document.getElementById("telefono").value;
+
+    let clienteNuevo = new Cliente(nombre, apellido, añoNacimiento, telefono)
+    
+    listaClientes.push(clienteNuevo);
+    localStorage.setItem("clientes", JSON.stringify(clientesAgregados));
+    
+    return clientesAgregados;
 }
 
-//Mensajes que quiero que arroje la consola
-
-nombreCompleto = `El nombre completo del cliente es ${nombre} ${apellido}. Tiene ${edad} años.`;
-
-console.log(nombreCompleto);
-
-
-pedidoCompleto = `El producto pedido es ${producto}, y la cantidad elegida es ${cantidadProducto}. El precio unitario es de $ ${precioProducto}.`
-
-console.log(pedidoCompleto);
-
-
-function precioFinal( cantidad, precio, desc ) {
-    precioPedido = cantidad * precio * desc
-}
-function mostrarMonto (mensaje) {
-    console.log(mensaje)
-}
-
-precioFinal ( cantidadProducto, precioProducto, descuento );
-mostrarMonto ( precioPedido );
-
-//Voy a iniciar un listado de los clientes, creándolos como objetos
-
-function Cliente(nombre, apellido, edad){
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.edad = edad;
-}
-
-function crearCliente(){
-    let nombre = prompt("¿Cuál es su nombre");
-    let apellido = prompt("¿Cuál es su apellido?");
-    let edad = parseInt(prompt("¿Cuántos años tiene?"));
-
-    const clienteNuevo = new Cliente(nombre, apellido, edad)
-
-    console.log(clienteNuevo)
-    return clienteNuevo;
+formulario.onsubmit = (e) => {
+    e.preventDefault();
+    crearCliente();
+    mostrarClientes()
 }
 
 
-let listaClientes = [{nombre: "Juan", apellido: "Pérez"}, {nombre: "Marta", apellido: "Gómez"}, {nombre: "María", apellido: "González"}, {nombre: "Pedro", apellido: "García"}, {nombre: "Lucas", apellido: "Gallo"}];
 
-console.log(listaClientes);
-console.log(listaClientes[4]);
-console.log(listaClientes[1].apellido);
 
-for (let i = 0; i < 5; i++){
-    console.log(listaClientes[i])
+//Quiero mostrarle al usuario en el HTML un resumen de su pedido(en el DOM)
+
+class Pedidos {
+    constructor(nombre, apellido, producto, cantidad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
 }
 
-let listaExclientes = [{nombre: "Nicolás", apellido: "Alberdi"}, {nombre: "Teresa", apellido: "Urroz"}];
-let listaFinal = listaClientes.concat(listaExclientes);
-console.log(listaFinal);
+const pedidosRecibidos = [];
 
-console.log(listaFinal.join(" / "));
-console.log(listaFinal.indexOf({nombre: "Federico", apellido: "López"}));
+const crearPedido = () => {
+    let nombre = document.querySelector("#nombre").value;
+    let apellido = document.querySelector("#apellido").value;
+    let producto = document.querySelector("#producto").value;
+    let cantidad = parseInt(document.querySelector("#cantidad").value);
+
+    let pedidoNuevo = new Pedidos(nombre, apellido, producto, cantidad);
+    pedidosRecibidos.push(pedidoNuevo);
 
 
+    return pedidosRecibidos
+}
 
-
-
-
+const mostrarPedido =
+pedidosRecibidos.forEach(pedido => {
+    let nodo = document.createElement("div");
+    nodo. innerHTML = `<h3>El pedido del cliente ${pedido.nombre} ${pedido.apellido} es ${pedido.cantidadProducto} unidades de ${pedido.producto}.</h3>`
+    document.getElementById("parrafoPedido").appendChild(nodo);
+})
 
